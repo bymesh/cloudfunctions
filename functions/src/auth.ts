389c520c81bde7516d7ceba49fea942c24355629
Kendy205 <tech@tomaskebrle.cz>
2021-11-27 20:17:22 +0100

    :test_tube: Added tests

--- functions/src/auth.ts
+++ functions/src/auth.ts
@@ -3,7 +3,6 @@ import * as admin from 'firebase-admin';
 export const app = admin.initializeApp();
 export const db = app.firestore();
 
-
 export const createUserRecord = functions.auth
     .user()
     .onCreate((user, context) => {
